ac488672a62b9c9ec411deb36a2918a80b2741c9
Sehan Mohammed <2000030708@kluniversity.in>
2022-12-26 16:50:24 +0530

    Update infinite_for_loop.c

--- infinite_for_loop.c
+++ infinite_for_loop.c
@@ -4,6 +4,6 @@ using namespace std;
 int main () {  
         for (; ;)    
           {    
-                  cout<<"Infinitive For Loop\n";    
+                  cout<<"Infinitive For Loop\n";    /wrojaouzgjn
           }    
     } 
